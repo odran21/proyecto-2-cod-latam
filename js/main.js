5cc2a62c569542d097d85995ce107cec647a7ed4
odran21 <jleonardoguevaram@gmail.com>
2023-05-31 01:11:33 -0500

    Cambio icono e Img -0MenuHamb. para cambiarlo

--- js/main.js
+++ js/main.js
@@ -1,55 +1,47 @@
-const hamburguesa = document.querySelector('.toggle');
-const menuMobile = document.querySelector('.menu-mobile');
+const iconoMenu = document.querySelector('#icono-menu');
+const menu = document.querySelector('#menu');
 
-hamburguesa.addEventListener('click', function() {
-  if (hamburguesa.classList.contains('open')) {
-    hamburguesa.classList.remove('open');
-  } else {
-    hamburguesa.classList.add('open');
-  }
+iconoMenu.addEventListener('click', function() {
+    menu.classList.toggle('active');
+    document.body.classList.toggle('opacity');
 });
-
-hamburguesa.addEventListener('click', function() {
-    hamburguesa.classList.toggle('open');
-    menuMobile.classList.toggle('open');
-  });
 
 //---------- Proyectos ----------//
 let proyectos = [
-    {
-        nombre: 'Futuro Proyecto',
-        tipo: 'Primer',
-        img: 'https://i0.wp.com/loquesigue.tv/wp-content/uploads/2019/03/img-html.jpg?fit=720%2C405&ssl=1'
-    },
-
-    {
-        nombre: 'Futuro Proyecto',
-        tipo: 'Segundo',
-        img: 'https://i0.wp.com/loquesigue.tv/wp-content/uploads/2019/03/img-html.jpg?fit=720%2C405&ssl=1'
-    },
+  {
+    nombre: "Futuro Proyecto 1",
+    tipo: "Primer",
+    img: "https://i0.wp.com/loquesigue.tv/wp-content/uploads/2019/03/img-html.jpg?fit=720%2C405&ssl=1 ",
+  },
+  {
+    nombre: "Futuro Proyecto 2",
+    tipo: "Segundo",
+    img: "https://i0.wp.com/loquesigue.tv/wp-content/uploads/2019/03/img-html.jpg?fit=720%2C405&ssl=1",
+  },
 ];
 
-function crearHTMLProyectos(proyectos) {
-    let nombreProyecto = document.createElement('h3');
-    nombreProyecto.innerText = proyectos.nombre;
-  
-    let imagenProyectos = document.createElement('img');
-    imagenProyectos.src = proyectos.img;
-    
-    let tipoProyectos = document.createElement('p');
-    tipoProyectos.innerText = proyectos.tipo;
-    
-    let listItem = document.createElement('li');
-    listItem.appendChild(nombreProyectos);
-    listItem.appendChild(imagenProyectos);
-    listItem.appendChild(tipoProyectos); 
-    
-    return listItem;
-  }
+function crearHTMLProyectos(proyecto) {
+  let nombreProyecto = document.createElement("h3");
+  nombreProyecto.innerText = proyecto.nombre;
 
-  const listaProyectos = document.querySelector('#proyectos-list');
+  let imagenProyecto = document.createElement("img");
+  imagenProyecto.src = proyecto.img;
+  imagenProyecto.alt = proyecto.nombre;
 
-  proyectos.forEach(function(proyectos) {
-  let proyectosListItem = crearHTMLProyectos(proyectos)
-    listaProyectos.appendChild(proyectosListItem);
-  });
+  let tipoProyecto = document.createElement("p");
+  tipoProyecto.innerText = proyecto.tipo;
+
+  let listItem = document.createElement("li");
+  listItem.appendChild(nombreProyecto);
+  listItem.appendChild(imagenProyecto);
+  listItem.appendChild(tipoProyecto);
+
+  return listItem;
+}
+
+const listaProyectos = document.querySelector("#proyectos-list");
+
+proyectos.forEach(function (proyecto) {
+  let proyectoListItem = crearHTMLProyectos(proyecto);
+  listaProyectos.appendChild(proyectoListItem);
+});
